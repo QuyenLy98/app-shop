555a4be20692855da146a1a8a3d72c6e8bc48be1
QuyenLy98 <132859298+QuyenLy98@users.noreply.github.com>
2023-05-18 21:14:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
